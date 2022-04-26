f61d0434024def1be5777b1d9e4d4a1b3b28ab92
Elias Segundo <elias.segundo@luisrazo.local>
2022-04-26 16:55:16 -0500

    Adding restriction

--- inventory.groovy
+++ inventory.groovy
@@ -33,8 +33,8 @@
      parameters {
        nodeParam('TEST_HOST') {
          description("Select test host slave")
-         defaultNodes(['35.224.32.77'])
-         allowedNodes(['35.224.32.77'])
+         defaultNodes(['35.224.32.77','slave'])
+         allowedNodes(['35.224.32.77','slave'])
          trigger('multiSelectionDisallowed')
          eligibility('IgnoreOfflineNodeEligibility')
        }
