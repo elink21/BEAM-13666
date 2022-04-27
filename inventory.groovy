76fc5fb0cafd90ffc419b043455c8c56b433a9ff
Elias Segundo <elias.segundo@luisrazo.local>
2022-04-27 09:48:09 -0500

    Testing removal

--- inventory.groovy
+++ inventory.groovy
@@ -15,6 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import hudson.model.*
+import jenkins.model.Jenkins
 
 
 
@@ -42,5 +44,6 @@
 
     steps {
       shell('echo job succeded')
+      shell("${Jenkins.instance.queue}")
     }
   }
